f4219d92dbc6b8f64efb186a87ae0d66edb92a6d
Ivan <ivan_junior_we@hotmail.com>
2024-10-23 16:43:20 -0300

    Criando o Desafio04

---
+++ stream-api/src/Desafio04.java
@@ -0,0 +1,11 @@
+public class Desafio04 {
+    public static void main(String[] args) {
+        
+        /**
+         * Desafio 4 - Remova todos os valores ímpares:
+         * Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
+         */
+
+        
+    }
+}
