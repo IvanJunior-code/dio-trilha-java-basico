f91df9e084eb859c7fb8e2fd46eaa3b328c8f047
Ivan <ivan_junior_we@hotmail.com>
2024-10-23 17:57:00 -0300

    Criando Desafio06

---
+++ stream-api/src/Desafio06.java
@@ -0,0 +1,14 @@
+
+public class Desafio06 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Desafio 6 - Verificar se a lista contém algum número maior que 10:
+         * Utilize a Stream API para verificar se a lista contém algum número
+         * maior que 10 e exiba o resultado no console.
+         */
+
+        
+    }
+}
