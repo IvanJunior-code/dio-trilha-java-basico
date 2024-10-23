76d4821c45b70a4c497c826774a6580b2a8c33ad
Ivan <ivan_junior_we@hotmail.com>
2024-10-23 16:21:44 -0300

    Corrigindo com o método allMatch e armazenando em uma variavel boolean o resultado se a lista contem somente números positivos ou não.

--- stream-api/src/Desafio03.java
+++ stream-api/src/Desafio03.java
@@ -13,8 +13,12 @@ public class Desafio03 {
          */
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        numeros.stream()
-                .filter(n -> n > 0)
-                .forEach(System.out::println);
+        // numeros.stream()
+        //         .filter(n -> n > 0)
+        //         .forEach(System.out::println);
+
+        boolean positivo = numeros.stream().allMatch(n -> n > 0);
+
+        System.out.println(positivo);
     }
 }
