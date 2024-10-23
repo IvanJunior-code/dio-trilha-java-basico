ea63d7c9f598994134928ac09e4d368a13ca3f51
Ivan <ivan_junior_we@hotmail.com>
2024-10-23 17:12:40 -0300

    Filtrando os números pares e coletando eles para uma nova lista.

--- stream-api/src/Desafio04.java
+++ stream-api/src/Desafio04.java
@@ -1,11 +1,24 @@
-public class Desafio04 {
-    public static void main(String[] args) {
-        
-        /**
-         * Desafio 4 - Remova todos os valores ímpares:
-         * Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
-         */
 
-        
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio04 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Desafio 4 - Remova todos os valores ímpares: Utilize a Stream API
+         * para remover os valores ímpares da lista e imprima a lista resultante
+         * no console.
+         */
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> pares = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(pares);
+
     }
 }
