fb4fb35a7ad30bcc45bc706291bc89502086afc3
Ivan <ivan_junior_we@hotmail.com>
2024-10-24 10:21:53 -0300

    Implementada verificação se a lista está vazia, ou se possui mais que 1 número

--- stream-api/src/Desafio07.java
+++ stream-api/src/Desafio07.java
@@ -12,14 +12,21 @@ public class Desafio07 {
          * Stream API, encontre o segundo número maior da lista e exiba o
          * resultado no console.
          */
+        
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         List<Integer> listaOrdenada = numeros.stream()
                 .distinct()
                 .sorted()
                 .collect(Collectors.toList());
-
-        System.out.println(listaOrdenada.get(listaOrdenada.size() - 2));
+        
+        if (!listaOrdenada.isEmpty()) {
+            if (listaOrdenada.size() >= 2) {
+                System.out.println("O segundo maior número da lista é o " + listaOrdenada.get(listaOrdenada.size() - 2) + ".");
+            } else {
+                System.out.println("A lista possui somente o número " + listaOrdenada.get(0) + ".");
+            }
+        }
 
     }
 }
