2c9479864f969e8de7fc576147f2a9f27df99108
Ivan <ivan_junior_we@hotmail.com>
2024-10-24 09:47:10 -0300

    Simplificando com o método anyMatch()

--- stream-api/src/Desafio06.java
+++ stream-api/src/Desafio06.java
@@ -13,9 +13,11 @@ public class Desafio06 {
          */
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
+        // boolean possuiMaiorQueDez = numeros.stream()
+        //         .filter(n -> n > 10)
+        //         .count() > 0;
         boolean possuiMaiorQueDez = numeros.stream()
-                .filter(n -> n > 10)
-                .count() > 0;
+                .anyMatch(n -> n > 10);
         
         System.out.println("A lista possui algum número maior que 10? " + possuiMaiorQueDez);
     }
