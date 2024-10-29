b212e79780520bba600433005385cf43374d4577
Ivan <ivan_junior_we@hotmail.com>
2024-10-29 11:41:28 -0300

    Testando a Classe PessoaFisica

--- lab-banco-digital-oo/src/edu/bancodigital/App.java
+++ lab-banco-digital-oo/src/edu/bancodigital/App.java
@@ -1,10 +1,22 @@
 package edu.bancodigital;
 
+import edu.bancodigital.cliente.PessoaFisica;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Cliente cliente1 = new Cliente();
-        // cliente1.setNome("Cliente 1");
+        PessoaFisica cliente1 = new PessoaFisica("Cliente 1 Pessoa Fisica", "Endereço do cliente 1", "111.111.111-11");
+        PessoaFisica cliente2 = new PessoaFisica("Cliente 2 Pessoa Fisica", "Endereço do cliente 2", "222.222.222-22");
+        
+        System.out.println("CPF: " + cliente1.getCPF() + ".");
+        System.out.println("Nome: " + cliente1.getNome() + ".");
+        System.out.println("Endereço: " + cliente1.getEndereco() + ".");
+
+        System.out.println("CPF: " + cliente2.getCPF() + ".");
+        System.out.println("Nome: " + cliente2.getNome() + ".");
+        System.out.println("Endereço: " + cliente2.getEndereco() + ".");
+        
+        
 
         // Conta contaCorrente = new ContaCorrente(cliente1);
         // contaCorrente.imprimirSaldo();
@@ -22,6 +34,8 @@ public class App {
         
         // contaPoupanca.imprimirSaldo();
         // contaCorrente.imprimirSaldo();
+
+
 
     }
 }
